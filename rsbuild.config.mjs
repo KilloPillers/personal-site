d62cf773db712828ec886641071f4b0ab0566c3d
Juan Alvarez <KilloPillers@hotmail.com>
2025-05-19 17:13:05 -0700

    added metadata

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -7,4 +7,18 @@ export default defineConfig({
     RSBUILD_DEV_API_URL: import.meta.env.RSBUILD_DEV_API_URL,
     RSBUILD_PROD_API_URL: import.meta.env.RSBUILD_PROD_API_URL,
   },
+  html: {
+    title: 'Juan Alvarez Portfolio',
+    meta: [
+      {
+        description:
+          'Full-stack Software Developer, Project Manager, Game Developer... find out more inside!',
+      },
+      {
+        image:
+          'https://jfalvarez-portfolio-assets.s3.us-east-2.amazonaws.com/pfp.jpg',
+      },
+      { type: 'website' },
+    ],
+  },
 });
